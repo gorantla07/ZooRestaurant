a146452769a75801bed7acd81d99dcf7d4c0edc8
gorantla07 <s545503@nwmissouri.edu>
2021-10-30 15:19:02 -0500

    Initial Push

---
+++ src/main/java/edu/nwmissouri/zoo02group/ZooRestaurantApplication.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.zoo02group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/nwmissouri/zoo02group/ZooRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.zoo02group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
