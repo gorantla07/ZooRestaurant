f4999413b70f7ea38f6f30c7c4f2808accc3fb64
gorantla07 <s545503@nwmissouri.edu>
2021-11-05 11:34:49 -0500

    Added comments to the visitor and staff classes.

--- src/main/java/edu/nwmissouri/zoo02group/Visitor.java
+++ src/main/java/edu/nwmissouri/zoo02group/Visitor.java
@@ -8,12 +8,20 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Pavan Kumar Atmakuri
+ * @author Manoj Gorantla
  */
 public class Visitor extends Person {
     int visitorId;
     private static ArrayList<Visitor> visitorList=new ArrayList<>();
     
+    /**
+     * 
+     * @param vistorId
+     * @param firstName
+     * @param lastName
+     * @param age
+     * @param gender 
+     */
     public Visitor(int vistorId,String firstName, String lastName, int age, String gender) {
         super(firstName, lastName, age, gender);
         this.visitorId=vistorId;
@@ -22,13 +30,25 @@ public class Visitor extends Person {
 
     public Visitor(){}
 
+    /**
+     * 
+     * @return visitor Id
+     */
     public int getVisitorId() {
         return visitorId;
     }
-
+    
+    /**
+     * 
+     * @param visitorId 
+     */
     public void setVisitorId(int visitorId) {
         this.visitorId = visitorId;
     }
+    
+    /**
+     * Added the visitors
+     */
    public static void creat(){  
        visitorList.add(new Visitor(101,"Shashi","pedha",22,"Male"));
        visitorList.add(new Visitor(102,"Manoj","Gorantla",26,"Male"));
@@ -36,11 +56,17 @@ public class Visitor extends Person {
        visitorList.add(new Visitor(104,"Madhava","Nelluri",22,"Male"));
    }
     
-
+   /**
+    * 
+    * @return visitor list
+    */
     public static ArrayList<Visitor> getVisitorList() {
         return visitorList;
     }
-
+    /**
+     * 
+     * @param vis 
+     */
     public void setVisitorList(Visitor vis) {
         visitorList.add(vis);
     }
